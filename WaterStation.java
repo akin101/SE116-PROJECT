f99f3bc07510523ed10dfb83fd287c3f5eae1465
Yiğit Ege Doğan <yigitegedogan@hotmail.com>
2026-05-31 00:26:05 +0300

    Update WaterStation.java

--- WaterStation.java
+++ WaterStation.java
@@ -1,6 +1,13 @@
-public class WaterStation extends UtilityProvider {
+class WaterStation extends UtilityProvider {
 
     public WaterStation(int row, int col) {
         super(row, col, 'W', "water");
+    }
+}
+
+class InternetHub extends UtilityProvider {
+
+    public InternetHub(int row, int col) {
+        super(row, col, 'T', "internet");
     }
 }
