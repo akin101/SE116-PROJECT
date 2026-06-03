c90a1e9c6ac6c7a1dc0f3725e5d3f78d9c3a3ce2
Yiğit Ege Doğan <yigitegedogan@hotmail.com>
2026-06-04 00:22:35 +0300

    Update PowerPlant.java

--- PowerPlant.java
+++ PowerPlant.java
@@ -5,16 +5,3 @@ class PowerPlant extends UtilityProvider {
     }
 }
 
-class WaterStation extends UtilityProvider {
-
-    public WaterStation(int row, int col) {
-        super(row, col, 'W', "water");
-    }
-}
-
-class InternetHub extends UtilityProvider {
-
-    public InternetHub(int row, int col) {
-        super(row, col, 'T', "internet");
-    }
-}
