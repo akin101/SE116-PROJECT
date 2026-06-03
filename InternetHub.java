193f650c54376c507ce267b5519725d093235105
Yiğit Ege Doğan <yigitegedogan@hotmail.com>
2026-06-04 00:21:47 +0300

    Update InternetHub.java

--- InternetHub.java
+++ InternetHub.java
@@ -5,28 +5,5 @@ class InternetHub extends UtilityProvider {
     }
 }
 
-abstract class ServiceBuilding extends Cell {
 
-    private final String serviceType;
-    private final int radius;
 
-    protected ServiceBuilding(int row, int col, char symbol,
-                              String serviceType, int radius) {
-        super(row, col, symbol);
-        this.serviceType = serviceType;
-        this.radius = radius;
-    }
-
-    public String getServiceType() {
-        return serviceType;
-    }
-
-    public int getRadius() {
-        return radius;
-    }
-
-    @Override
-    public boolean isConnectable() {
-        return false;
-    }
-}
